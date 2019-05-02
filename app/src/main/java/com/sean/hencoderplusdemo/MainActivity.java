054b4b364932b2346433f575bd55758c56fa5561
wenpingkk <wenpingkk@163.com>
2019-05-02 14:18:08 +0800

    001

---
+++ app/src/main/java/com/sean/hencoderplusdemo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.sean.hencoderplusdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Author WenPing
+ * CreateTime 2019/5/2.
+ * Description:
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
