5a310efd66a7bcc6d2801ce8fc3266ca877d7f16
wenpingkk <wenpingkk@163.com>
2019-05-04 14:09:36 +0800

    008 CameraView 使用PropertyHolder

--- animation_08/src/main/java/com/sean/animation_08/MainActivity.java
+++ animation_08/src/main/java/com/sean/animation_08/MainActivity.java
@@ -1,7 +1,8 @@
 package com.sean.animation_08;
 
-import android.animation.AnimatorSet;
+
 import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -21,16 +22,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void camereView() {
-        AnimatorSet animatorSet = new AnimatorSet();
+/*        AnimatorSet animatorSet = new AnimatorSet();
         ObjectAnimator bottomFlip = ObjectAnimator.ofFloat(view, "bottomFlip", 45);
         bottomFlip.setDuration(1500);
-        ObjectAnimator flipRotation = ObjectAnimator.ofFloat(view, "flipRotation",  20);
+        ObjectAnimator flipRotation = ObjectAnimator.ofFloat(view, "flipRotation",  270);
         flipRotation.setDuration(1500);
-        ObjectAnimator topFlipAnimator = ObjectAnimator.ofFloat(view, "topFlip", 0);
+        ObjectAnimator topFlipAnimator = ObjectAnimator.ofFloat(view, "topFlip", -45);
         topFlipAnimator.setDuration(1500);
         animatorSet.playSequentially(bottomFlip,flipRotation,topFlipAnimator);
         animatorSet.setDuration(3000);
-        animatorSet.start();
+        animatorSet.start();*/
+        PropertyValuesHolder bottomFlipHolder = PropertyValuesHolder.ofFloat("bottomFlip",45);
+        PropertyValuesHolder flipRotationHolder = PropertyValuesHolder.ofFloat("flipRotation",270);
+        PropertyValuesHolder topFlipHolder = PropertyValuesHolder.ofFloat("topFlip",-45);
+        ObjectAnimator objectAnimator =  ObjectAnimator.ofPropertyValuesHolder(view,bottomFlipHolder, flipRotationHolder, topFlipHolder);
+        objectAnimator.setStartDelay(1000);
+        objectAnimator.setDuration(2000);
+        objectAnimator.start();
     }
 
 /*    private void circleView() {
