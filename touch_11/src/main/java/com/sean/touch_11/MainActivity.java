d9fe967a11243bdec9c8cd3d81e95cb61102dc7f
wenpingkk <wenpingkk@163.com>
2019-05-14 15:20:37 +0800

    034 test touch

--- touch_11/src/main/java/com/sean/touch_11/MainActivity.java
+++ touch_11/src/main/java/com/sean/touch_11/MainActivity.java
@@ -12,35 +12,51 @@ public class MainActivity extends AppCompatActivity {
 
     private String TAG = "mTag";
     TestView mTestView;
+    TestView mTestView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTestView = findViewById(R.id.view);
+        mTestView2 = findViewById(R.id.view2);
         mTestView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                Log.i(TAG, MainActivity.class.getSimpleName() + ":setOnTouchListener:mTestView");
+                Log.e(TAG, MainActivity.class.getSimpleName() + ":setOnTouchListener:mTestView");
                 return false;
             }
         });
         mTestView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i(TAG, MainActivity.class.getSimpleName() + ":setOnClickListener:mTestView");
+                Log.e(TAG, MainActivity.class.getSimpleName() + ":setOnClickListener:mTestView");
+            }
+        });
+
+        mTestView2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.e(TAG, MainActivity.class.getSimpleName() + ":setOnTouchListener:mTestView2");
+                return false;
+            }
+        });
+        mTestView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e(TAG, MainActivity.class.getSimpleName() + ":setOnClickListener:mTestView2");
             }
         });
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        Log.i(TAG, MainActivity.class.getSimpleName() + ":onTouchEvent:" + event.getAction());
+        Log.e(TAG, MainActivity.class.getSimpleName() + ":onTouchEvent:" + event.getAction());
         return super.onTouchEvent(event);
     }
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
-        Log.i(TAG, MainActivity.class.getSimpleName() + ":dispatchTouchEvent:" + ev.getAction());
+        Log.e(TAG, MainActivity.class.getSimpleName() + ":dispatchTouchEvent:" + ev.getAction());
         return super.dispatchTouchEvent(ev);
     }
 }
